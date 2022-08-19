37f693fc90f773ec4d1f373bd2d6d1e69dedebc3
Aleksey_Jyravlev <319and8@gmail.com>
2022-08-19 20:21:43 +0300

    dz_seminar_16.08.22

---
+++ DZ/dz_seminar_16.08.22/task_25/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A
+ в натуральную степень B.
+2, 4 -> 16
+*/
+
+int [] get_input_numbers()
+{
+int [] input_numbers = new int [2];
+while(true)
+    {
+        try
+        {
+            Console.WriteLine("Введите число 'A', которое будет возводиться в степень: ");
+            input_numbers[0] = int.Parse(Console.ReadLine() ?? "");
+
+            Console.WriteLine("Введите число 'B', степень в которую возведём число А: ");
+            input_numbers[1] = int.Parse(Console.ReadLine() ?? "");
+        
+            return input_numbers;
+        }
+        
+        catch( Exception error )
+        {
+            Console.WriteLine($"{error.Message}, введите целые числа");
+            continue;
+        }
+    }
+}
+
+double get_exponentiation(int [] array)
+{
+    double result = 1;
+    if (array[1] == 0)
+        return result;
+    else if (array[1] > 0)
+    {
+        while (array[1] > 0)
+        {
+            result *= array[0];
+            array[1]--;
+        }
+        return result;
+    }
+    else
+    {
+        int i = Math.Abs(array[1]);
+        int denominator = 1;
+        while (i > 0)
+        {
+            denominator *= array[0];
+            i--;
+        }
+        result = result/denominator;
+        return result;
+    }
+} 
+
+int [] input_numbers = get_input_numbers();
+Console.WriteLine($"Число A = {input_numbers[0]} в степени B = {input_numbers[1]}, равняется: {get_exponentiation(input_numbers)}");
---
+++ DZ/dz_seminar_16.08.22/task_29/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int [] input_elements_array()
+{
+    int [] array = new int [8];
+    while(true)
+    {
+        try
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введиnt элемент массива №{i}: ");
+                array[i] = int.Parse(Console.ReadLine() ?? "");
+            }
+            return array;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка ввода. Введите целые числа");
+        }
+    }
+
+}
+
+void output_elements_array(int [] array)
+{
+    Console.WriteLine(array);
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Элемент массива №{i} равняется {array[i]}");
+    }
+}
+output_elements_array(input_elements_array());
