6e0ca09e9f2d7c956c9bee4d21d93b1e4e1204ef
Aleksey_Jyravlev <319and8@gmail.com>
2022-08-26 14:22:06 +0300

    dz_23.08.22

---
+++ DZ/dz_seminar_23.08.22/task_41/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл 
+пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int count_numbers() // Ввод длины массива(количество чисел для проверки)
+{
+    while(true)
+    {
+        try
+        {  
+       
+            Console.WriteLine("Введите количество чисел которые вы хотите проверить:");
+            int count_numbers = int.Parse(Console.ReadLine() ?? "");
+            if (count_numbers < 0)
+            {
+                Console.WriteLine("Необходимо целое положительное число");
+                continue;
+            }
+            return count_numbers;
+        }
+
+        catch (Exception)
+        {
+            Console.WriteLine("Необходимо ввести целое число");
+        }
+    }
+}
+
+int [] creating_array(int length) // Создание массива
+{
+    int [] creating_array = new int [length];
+    return creating_array;
+}
+
+int [] fill_array(int [] array) // Заполнение массива
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        try
+        {
+            Console.Write($"Введите число №{i+1}: ");
+            array[i] = int.Parse(Console.ReadLine() ?? "");
+            i++;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Необходимо ввести целое число");
+            i = 0;
+        }
+    }
+    return array;
+}
+ 
+int count_positive_numbers(int [] array) // Подсчёт положительных элементов
+{
+    int count = 0;
+    foreach (int element in array)
+    {
+        if (element > 0)
+            count++;
+    }
+    return count;
+}
+
+int [] array = fill_array(creating_array(count_numbers()));
+int result = count_positive_numbers(array);
+Console.WriteLine($"Количество положительных чисел равно: {result}");
+
---
+++ DZ/dz_seminar_23.08.22/task_43/Program.cs
@@ -0,0 +1,43 @@
+﻿/* 
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+ y = k1 * x + b1
+ y = k2 * x + b2
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double[] input_value()
+{
+    double[] array = new double[4];
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введие значение к1: ");
+            array[0] = double.Parse(Console.ReadLine() ?? "");
+
+            Console.Write("Введие значение к2: ");
+            array[1] = double.Parse(Console.ReadLine() ?? "");
+
+            Console.Write("Введие значение b1: ");
+            array[2] = double.Parse(Console.ReadLine() ?? "");
+
+            Console.Write("Введие значение b2: ");
+            array[3] = double.Parse(Console.ReadLine() ?? "");
+            return array;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Вводите вещественные числа!");
+        }
+    }
+}
+
+void result(double[] array)
+{
+    double x = (array[3] - array[2]) / (array[0] - array[1]);
+    double y = array[0] * x + array[2];
+    Console.WriteLine($"{(x, y)}");
+}
+
+result(input_value());
