7a24f6f60a1a192fcbb9e3949ba377b0169715f5
Aleksey_Jyravlev <319and8@gmail.com>
2022-09-06 08:58:41 +0300

    dz_02.09.22

---
+++ DZ/dz_seminar_02.09.22/task_64/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа 
+в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+
+string output_numbers_range(int m, int n)
+{
+    string result = string.Empty;
+    if ( m <= n)
+    {
+        return $"{result + m.ToString()}, " + output_numbers_range(m+1, n);
+    }
+    else 
+    {
+        return string.Empty;
+    }
+}
+string result = output_numbers_range(5,8);
+Console.Write(result.Substring(0,result.Length-2));
---
+++ DZ/dz_seminar_02.09.22/task_66/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int sum_element_range( int start, int end)
+{
+    int sum = 0;
+    if (start <= end ) 
+    {
+        return sum +=start + sum_element_range(start+1,end);
+    }
+    else
+        return 0;
+}
+Console.Write(sum_element_range(1,15));
