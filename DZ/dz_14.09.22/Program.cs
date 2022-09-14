735fb76b61096244c8728d61a6c33ddaa4920f9a
Aleksey_Jyravlev <319and8@gmail.com>
2022-09-14 15:11:08 +0300

    dz_14.09.22

---
+++ DZ/dz_14.09.22/Program.cs
@@ -0,0 +1,25 @@
+﻿string [] search_array(string [] array)
+{
+    int count = 0;
+    foreach(string i in array) // Перебор массива для понимания какой длины массив создать
+    {
+        if (i.ToString().Length <= 3) count++;
+    }
+    string [] result_array = new string [count];
+
+    int j = 0;
+    foreach(string i in array) // вывод на экран
+    {
+        if (i.ToString().Length <= 3)
+        {
+            result_array[j]=i;
+            Console.Write($"{result_array[j]} ");
+            j++;
+        }
+    }
+    return result_array;
+}
+
+string[] array = {"hello", "world", ":-)", "1234", "-2", };
+string[] result = search_array(array);
+
